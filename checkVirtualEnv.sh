b27c900a1564abbea1899c6bb2d43fa3fc05b240
Don Drake <don@drakeconsulting.com>
2016-01-19 21:20:46 -0600

    virtualenv

---
+++ checkVirtualEnv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+
+MYSRCDIR=$( cd -P $(dirname ${BASH_SOURCE[0]}); echo $PWD )
+export VIRTDIR=$MYSRCDIR/PY27
+
+if [ ! -d $MYSRCDIR/PY27 ]; then
+
+    /usr/local/bin/virtualenv $VIRTDIR
+    source $VIRTDIR/bin/activate
+    pip install -r $MYSRCDIR/requirements.txt
+fi
+
+source $VIRTDIR/bin/activate
+unset MYSRCDIR
+
