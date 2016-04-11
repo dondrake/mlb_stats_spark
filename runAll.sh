a259bafa7d388ccc9824749f22aba3b9f60dc992
Don Drake <don@drakeconsulting.com>
2016-04-10 22:25:47 -0500

    added git pull

--- runAll.sh
+++ runAll.sh
@@ -5,6 +5,8 @@ SRCDIR=$( cd -P $(dirname ${BASH_SOURCE[0]}); echo $PWD )
 source $SRCDIR/checkVirtualEnv.sh
 source $SRCDIR/spark_config.sh
 
+cd $SRCDIR
+git pull
 
 /bin/date
 echo "running getMLBStats.sh"
