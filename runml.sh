0d689bfd93d26ca6baf9aade8df55361cc17eada
Don Drake <don@drakeconsulting.com>
2016-05-09 10:44:37 -0500

    add datestamp to runml output

--- runml.sh
+++ runml.sh
@@ -5,6 +5,8 @@ SRCDIR=$( cd -P $(dirname ${BASH_SOURCE[0]}); echo $PWD )
 source $SRCDIR/checkVirtualEnv.sh
 source $SRCDIR/spark_config.sh
 
+NOW=$(date +"%Y%m%d%H%M%S")
+
 time spark-submit  \
 --verbose \
 --master local[*] \
@@ -14,7 +16,7 @@ time spark-submit  \
 --conf "spark.eventLog.enabled=true" \
 --conf "spark.eventLog.dir=$EVENT_LOG_DIR" \
 --conf "spark.local.dir=$LOCALDIR" \
-$SRCDIR/TrainModel.py > $SRCDIR/runml.out 2> $SRCDIR/runml.err
+$SRCDIR/TrainModel.py > $SRCDIR/runml.${NOW}.out 2> $SRCDIR/runml.${NOW}.err
 RETVAL=$?
 
 echo "status $RETVAL"
